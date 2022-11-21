571c587e5be615a4206414641b269a6b60e24d8b
carlos-schmidt <carlosschmidt@protonmail.com>
2022-11-21 15:53:26 +0100

    Add warning if unknown agreement data is incoming

--- edc-extension4aas/src/main/java/de/fraunhofer/iosb/app/client/dataTransfer/DataTransferObservable.java
+++ edc-extension4aas/src/main/java/de/fraunhofer/iosb/app/client/dataTransfer/DataTransferObservable.java
@@ -1,10 +1,16 @@
 package de.fraunhofer.iosb.app.client.dataTransfer;
 
+import static java.lang.String.format;
+
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 
+import de.fraunhofer.iosb.app.Logger;
+
 public class DataTransferObservable {
+
+    private static final Logger LOGGER = Logger.getInstance();
 
     private final Map<String, CompletableFuture<String>> observers;
 
@@ -19,8 +25,14 @@ public class DataTransferObservable {
     public void unregister(String agreementId) {
         observers.remove(agreementId);
     }
-    
-    public void update(String agreementId, String data){
+
+    public void update(String agreementId, String data) {
+        if (!observers.containsKey(agreementId)) {
+            LOGGER.warn(format(
+                    "A POST request to the client's data transfer endpoint with an unknown agreementID was caught. AgreementID: %s",
+                    agreementId));
+            return;
+        }
         observers.get(agreementId).complete(data);
     }
 }

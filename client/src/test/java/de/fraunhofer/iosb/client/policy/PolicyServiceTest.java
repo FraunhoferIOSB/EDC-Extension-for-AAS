ffe9908f4ae0880508b2fb2f17259e5fa955b660
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-02-25 17:21:25 +0100

    Write new tests for PolicyService

--- client/src/test/java/de/fraunhofer/iosb/client/policy/PolicyServiceTest.java
+++ client/src/test/java/de/fraunhofer/iosb/client/policy/PolicyServiceTest.java
@@ -15,6 +15,7 @@
  */
 package de.fraunhofer.iosb.client.policy;
 
+import de.fraunhofer.iosb.client.exception.AmbiguousOrNullException;
 import de.fraunhofer.iosb.client.testUtils.FileManager;
 import org.eclipse.edc.catalog.spi.Catalog;
 import org.eclipse.edc.catalog.spi.DataService;
@@ -24,86 +25,180 @@ import org.eclipse.edc.connector.spi.catalog.CatalogService;
 import org.eclipse.edc.policy.model.Policy;
 import org.eclipse.edc.spi.EdcException;
 import org.eclipse.edc.spi.monitor.Monitor;
+import org.eclipse.edc.spi.query.QuerySpec;
+import org.eclipse.edc.spi.response.ResponseStatus;
 import org.eclipse.edc.spi.response.StatusResult;
 import org.eclipse.edc.spi.result.Result;
 import org.eclipse.edc.spi.system.configuration.ConfigFactory;
+import org.eclipse.edc.spi.types.domain.asset.Asset;
 import org.eclipse.edc.transform.spi.TypeTransformerRegistry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static java.lang.String.format;
+import static org.eclipse.edc.protocol.dsp.spi.types.HttpMessageProtocol.DATASPACE_PROTOCOL_HTTP;
+import static org.eclipse.edc.spi.query.Criterion.criterion;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+/**
+ * We assume here that catalogService does not return null objects as well as null catalogs inside their return value.
+ * Also, we assume that catalogs are valid JSON and expandable by the connector's JSON LD expander TitaniumJsonLd.class.
+ * Finally, we assume that catalogs can be transformed with the TypeTransformerRegistry.
+ */
 public class PolicyServiceTest {
 
     private final int providerPort = 54321;
-    private final CatalogService mockCatalogService = mock(CatalogService.class);
-    private final TypeTransformerRegistry mockTransformer = mock(TypeTransformerRegistry.class);
-
+    private CatalogService catalogService;
+    private TypeTransformerRegistry typeTransformerRegistry;
+    private PolicyServiceConfig config;
     private PolicyService policyService;
-    private final URL testUrl = new URL("http://localhost:" + providerPort);
+    private final URL testUrl;
 
     public PolicyServiceTest() throws MalformedURLException {
+        testUrl = new URL("http://localhost:" + providerPort);
     }
 
+
     @BeforeEach
-    void initializeContractOfferService() {
-        policyService = new PolicyService(mockCatalogService, mockTransformer, mockConfig(),
-                mock(PolicyDefinitionStore.class), mock(Monitor.class));
+    void initializePolicyService() {
+        catalogService = mock(CatalogService.class);
+        typeTransformerRegistry = mock(TypeTransformerRegistry.class);
+        var policyDefinitionStore = mock(PolicyDefinitionStore.class);
+        config = mock(PolicyServiceConfig.class);
+
+        policyService = new PolicyService(catalogService, typeTransformerRegistry, config, policyDefinitionStore, mock(Monitor.class));
     }
 
     @Test
-    void getPolicyForAssetIdTest() throws InterruptedException {
+    void getDatasetCatalogResponseFailureTest() throws InterruptedException {
+        var querySpec = QuerySpec.Builder.newInstance().filter(List.of(criterion(Asset.PROPERTY_ID, "=", "test-asset-id"))).build();
+        var future = new CompletableFuture<StatusResult<byte[]>>();
+        future.complete(StatusResult.failure(ResponseStatus.FATAL_ERROR, "This is a test"));
+
+        when(catalogService.requestCatalog("test-counter-party-id", testUrl.toString(), DATASPACE_PROTOCOL_HTTP, querySpec)).thenReturn(future);
+
+        try {
+            policyService.getDatasetForAssetId("test-counter-party-id", testUrl, "test-asset-id");
+            fail(); // Should throw exception
+        } catch (EdcException expected) {
+            assertEquals(format("Catalog by provider %s couldn't be retrieved: %s", testUrl, "[This is a test]"), expected.getMessage());
+        }
+    }
+
+    @Test
+    void getDatasetCatalogFutureTimeoutTest() throws InterruptedException {
+        var querySpec = QuerySpec.Builder.newInstance().filter(criterion(Asset.PROPERTY_ID, "=", "test-asset-id")).build();
+        var future = new CompletableFuture<StatusResult<byte[]>>();
+
+        when(catalogService.requestCatalog("test-counter-party-id", testUrl.toString(), DATASPACE_PROTOCOL_HTTP, querySpec)).thenReturn(future);
+
+        try {
+            policyService.getDatasetForAssetId("test-counter-party-id", testUrl, "test-asset-id");
+            fail(); // Should throw exception
+        } catch (EdcException expected) {
+            assertEquals(format("Timeout while waiting for catalog by provider %s.", testUrl), expected.getMessage());
+        }
+    }
+
+    @Test
+    void getDatasetNoDatasetsTest() throws InterruptedException {
+        var querySpec = QuerySpec.Builder.newInstance().filter(criterion(Asset.PROPERTY_ID, "=", "test-asset-id")).build();
+        var future = new CompletableFuture<StatusResult<byte[]>>();
+        var catalogString = FileManager.loadResource("catalog.json");
+        assert catalogString != null;
+        future.complete(StatusResult.success(catalogString.getBytes(StandardCharsets.UTF_8)));
+
+        when(catalogService.requestCatalog("test-counter-party-id", testUrl.toString(), DATASPACE_PROTOCOL_HTTP, querySpec)).thenReturn(future);
+        when(typeTransformerRegistry.transform(any(), any()))
+                .thenReturn(Result.success(Catalog.Builder.newInstance().build()));
+
+        try {
+            policyService.getDatasetForAssetId("test-counter-party-id", testUrl, "test-asset-id");
+            fail(); // Should throw exception
+        } catch (AmbiguousOrNullException expected) {
+            assertEquals(format("Multiple or no policyDefinitions were found for assetId %s!", "test-asset-id"), expected.getMessage());
+        }
+    }
+
+    @Test
+    void getDatasetTest() throws InterruptedException {
         var mockedFuture = new CompletableFuture<StatusResult<byte[]>>();
         var datasetId = "ef4d028f-70d7-404a-b22e-c5b0ffa3aa0b";
         var catalogString = FileManager.loadResource("catalog.json");
         assert catalogString != null;
         mockedFuture.complete(StatusResult.success(catalogString.getBytes(StandardCharsets.UTF_8)));
 
-        when(mockCatalogService.requestCatalog(any(), any(), any(), any())).thenReturn(mockedFuture);
+        when(catalogService.requestCatalog(any(), any(), any(), any())).thenReturn(mockedFuture);
 
-        when(mockTransformer.transform(any(), any())).thenReturn(Result.success(Catalog.Builder.newInstance()
-                .dataset(Dataset.Builder.newInstance()
-                        .id(datasetId)
-                        .offer(UUID.randomUUID().toString(),
-                                Policy.Builder.newInstance().build())
-                        .distribution(Distribution.Builder.newInstance()
-                                .dataService(DataService.Builder.newInstance().build())
-                                .format("")
-                                .build())
-                        .build())
-                .build()));
+        when(typeTransformerRegistry.transform(any(), any())).thenReturn(Result.success(Catalog.Builder.newInstance().dataset(Dataset.Builder.newInstance().id(datasetId).offer(UUID.randomUUID().toString(), Policy.Builder.newInstance().build()).distribution(Distribution.Builder.newInstance().dataService(DataService.Builder.newInstance().build()).format("").build()).build()).build()));
 
         assertEquals(datasetId, policyService.getDatasetForAssetId("provider", testUrl, "test-asset-id").getId());
     }
 
     @Test
-    void getContractUnreachableProviderTest() throws MalformedURLException, InterruptedException {
-        var mockedFuture = new CompletableFuture<StatusResult<byte[]>>();
-        when(mockCatalogService.requestCatalog(any(), any(), any(), any())).thenReturn(mockedFuture);
+    void getAcceptablePolicyForAssetIdTest() throws InterruptedException {
+        var shouldPolicy = Policy.Builder.newInstance().build();
+        var dataset = Dataset.Builder.newInstance()
+                .offer("test-offer-id", shouldPolicy).build();
+        // mock getDatasetMethod
+        var policyServiceSpy = spy(policyService);
+        Mockito.doReturn(dataset)
+                .when(policyServiceSpy)
+                .getDatasetForAssetId("test-counter-party-id", testUrl, "test-asset-id");
+        when(config.isAcceptAllProviderOffers()).thenReturn(true);
 
-        try {
-            policyService.getDatasetForAssetId("provider", new URL("http://fakeUrl:4321/not/working"), "test-asset-id");
-            fail("This should not complete without throwing an exception");
-        } catch (EdcException expected) {
-        }
+        var resultPolicy = policyServiceSpy.getAcceptablePolicyForAssetId("test-counter-party-id", testUrl, "test-asset-id");
+        assertEquals(shouldPolicy, resultPolicy.getSecond());
+        assertEquals("test-offer-id", resultPolicy.getFirst());
     }
 
-    private PolicyServiceConfig mockConfig() {
-        return new PolicyServiceConfig(ConfigFactory.fromMap(Map.of(
-                "edc.dsp.callback.address", "http://localhost:4321/dsp",
-                "web.http.port", "8080",
-                "web.http.path", "/api")));
+    @Test
+    void getAcceptablePolicyForAssetIdEmptyPolicyListTest() throws InterruptedException {
+        // mock getDatasetMethod
+        var policyServiceSpy = spy(policyService);
+        Mockito.doReturn(Dataset.Builder.newInstance().build()).when(policyServiceSpy).getDatasetForAssetId("test-counter-party-id", testUrl, "test-asset-id");
 
+        try {
+            policyServiceSpy.getAcceptablePolicyForAssetId("test-counter-party-id", testUrl, "test-asset-id");
+        } catch (EdcException expected) {
+            assertEquals("Could not find any acceptable policyDefinition", expected.getMessage());
+        }
+
+    }
+
+    @Test
+    void getAcceptablePolicyForAssetIdAcceptAllOffersTest() {
+        //TODO
+    }
+
+    @Test
+    void getAcceptablePolicyForAssetIdAcceptFromAcceptedListTest() {
+        //TODO
+    }
+
+    @Test
+    void getAcceptablePolicyForAssetIdNoAcceptablePolicyTest() {
+        //TODO
+    }
+
+    @Test
+    void getAcceptablePolicyForAssetIdTimeoutTest() {
+        //TODO
+    }
+
+    @Test
+    void getAcceptablePolicyForAssetIdExceptionbyGetDatasetTest() {
+        //TODO
     }
 }

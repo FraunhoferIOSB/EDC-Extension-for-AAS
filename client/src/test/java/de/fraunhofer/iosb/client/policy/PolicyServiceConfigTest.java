55f964812c10bf5b4d7b8ee99ae6b92e952e5db6
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-03-05 12:28:04 +0100

    Add policy service config test

---
+++ client/src/test/java/de/fraunhofer/iosb/client/policy/PolicyServiceConfigTest.java
@@ -0,0 +1,44 @@
+package de.fraunhofer.iosb.client.policy;
+
+import org.eclipse.edc.spi.system.configuration.Config;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PolicyServiceConfigTest {
+    private PolicyServiceConfig policyServiceConfig;
+    private Config config;
+
+    @BeforeEach
+    public void initialize() {
+        config = mock(Config.class);
+        policyServiceConfig = new PolicyServiceConfig(config);
+    }
+
+    @Test
+    public void getWaitForCatalogTimeoutTest() {
+        var expected = 42;
+        when(config.getInteger("waitForCatalogTimeout", 10)).thenReturn(expected);
+
+        assertEquals(expected, policyServiceConfig.getWaitForCatalogTimeout());
+    }
+
+    @Test
+    public void getAcceptedPolicyDefinitionsPathTest() {
+        var expected = "/tmp/test/policy-definitions/accepted/";
+        when(config.getString("acceptedPolicyDefinitionsPath", null)).thenReturn(expected);
+
+        assertEquals(expected, policyServiceConfig.getAcceptedPolicyDefinitionsPath());
+    }
+
+    @Test
+    public void isAcceptAllProviderOffersTest() {
+        var expected = true;
+        when(config.getBoolean("acceptedPolicyDefinitionsPath", false)).thenReturn(expected);
+
+        assertEquals(expected, policyServiceConfig.isAcceptAllProviderOffers());
+    }
+}

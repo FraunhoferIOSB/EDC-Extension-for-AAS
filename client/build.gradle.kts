f9ddad409b46d32afdb8465abc96961292585947
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-04-28 14:46:04 +0200

    Update to EDC v0.6.2

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -21,7 +21,8 @@ dependencies {
     implementation(project(":public-api-management"))
 
     // See this project's README.MD for explanations
-    implementation("$group:contract-core:$edcVersion")
+    implementation("$group:control-plane-contract:$edcVersion")
+    implementation("$group:json-ld-lib:$edcVersion")
     implementation("$group:dsp-catalog-http-dispatcher:$edcVersion")
     implementation("$group:management-api:$edcVersion")
     implementation("$group:runtime-metamodel:$edcVersion")

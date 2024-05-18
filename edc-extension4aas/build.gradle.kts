3078088836e1d008cc19703963f82342d2503087
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-05-18 12:59:57 +0200

    Update extension to fit edc-v0.6.3 Also update aas4j version to 1.0.2

--- edc-extension4aas/build.gradle.kts
+++ edc-extension4aas/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
 }
 
+val aas4jVersion: String by project
 val javaVersion: String by project
 val faaastVersion: String by project
 val edcVersion: String by project
@@ -29,8 +30,8 @@ dependencies {
     implementation("$group:data-plane-spi:$edcVersion") // DataSource, StreamResult
 
     implementation("de.fraunhofer.iosb.ilt.faaast.service:starter:${faaastVersion}")
-    implementation("org.eclipse.digitaltwin.aas4j:aas4j-dataformat-json:1.0.1")
-    implementation("org.eclipse.digitaltwin.aas4j:aas4j-model:1.0.1")
+    implementation("org.eclipse.digitaltwin.aas4j:aas4j-dataformat-json:${aas4jVersion}")
+    implementation("org.eclipse.digitaltwin.aas4j:aas4j-model:${aas4jVersion}")
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
 
     testImplementation("$group:junit:$edcVersion")

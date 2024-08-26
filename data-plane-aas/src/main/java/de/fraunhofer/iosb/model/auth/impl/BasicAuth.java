b404e4172e0e9d9f8afe966429160f726f48f65f
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-08-26 10:57:44 +0200

    Add AAS authentication types

---
+++ data-plane-aas/src/main/java/de/fraunhofer/iosb/model/auth/impl/BasicAuth.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 Fraunhofer IOSB, eine rechtlich nicht selbstaendige
+ * Einrichtung der Fraunhofer-Gesellschaft zur Foerderung der angewandten
+ * Forschung e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.fraunhofer.iosb.model.auth.impl;
+
+import de.fraunhofer.iosb.model.auth.AuthenticationMethod;
+import okhttp3.Credentials;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.AbstractMap;
+import java.util.Base64;
+import java.util.Map;
+
+/**
+ * <a href="https://datatracker.ietf.org/doc/html/rfc7617#section-2">rfc7617</a>
+ */
+public class BasicAuth extends AuthenticationMethod {
+
+    private final Base64.Encoder encoder = Base64.getEncoder();
+
+    private final String username;
+    private final String password;
+
+    public BasicAuth(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    protected String getValue() {
+        return "Basic %s".formatted(encoder.encodeToString("%s:%s".formatted(username, password).getBytes()));
+    }
+}

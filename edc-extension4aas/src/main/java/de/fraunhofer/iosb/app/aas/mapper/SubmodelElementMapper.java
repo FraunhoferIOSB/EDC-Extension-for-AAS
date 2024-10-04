86ee23625aa3afc03fe65dbeb9025ac1dd54ca96
carlos-schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-10-04 14:35:25 +0200

    Extract mapping logic to specific classes

---
+++ edc-extension4aas/src/main/java/de/fraunhofer/iosb/app/aas/mapper/SubmodelElementMapper.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2021 Fraunhofer IOSB, eine rechtlich nicht selbstaendige
+ * Einrichtung der Fraunhofer-Gesellschaft zur Foerderung der angewandten
+ * Forschung e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.fraunhofer.iosb.app.aas.mapper;
+
+import de.fraunhofer.iosb.dataplane.aas.spi.AasDataAddress;
+import org.eclipse.digitaltwin.aas4j.v3.model.Referable;
+import org.eclipse.digitaltwin.aas4j.v3.model.Reference;
+import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
+import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElementCollection;
+import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElementList;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultEmbeddedDataSpecification;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultReference;
+import org.eclipse.edc.connector.controlplane.asset.spi.domain.Asset;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Not an ElementMapper since we have a different method signature.
+ */
+class SubmodelElementMapper {
+
+    private final ElementMapper elementMapper;
+
+    public SubmodelElementMapper() {
+        this.elementMapper = new ElementMapper();
+    }
+
+    /* May contain traces of recursion */
+    <E extends SubmodelElement> Asset map(Reference parent, E submodelElement, String accessUrl) {
+        var reference = elementMapper.createReference(submodelElement.getIdShort(), parent);
+
+        var children = getContainerElements(submodelElement).stream()
+                .map(elem -> map(reference, elem, accessUrl))
+                .toList();
+
+        var dataAddress = AasDataAddress.Builder.newInstance()
+                .baseUrl(accessUrl)
+                .referenceChain(reference)
+                .build();
+
+        // Display the modeling type of the element, i.e. "Property"/"Operation"/...
+        // Get the implemented interface, else we will have Default<Name> instead of <Name>
+        var modelingType = Arrays.stream(submodelElement.getClass().getInterfaces())
+                .findFirst()
+                .map(Class::getSimpleName)
+                .orElse("SubmodelElement");
+
+        return elementMapper.mapReferableToAssetBuilder(submodelElement)
+                .id(elementMapper.getId(accessUrl, dataAddress))
+                .contentType("application/json")
+                .properties(Map.of(
+                        "embeddedDataSpecifications",
+                        Optional.ofNullable(submodelElement.getEmbeddedDataSpecifications())
+                                .orElse(List.of(new DefaultEmbeddedDataSpecification())),
+                        "semanticId",
+                        Optional.ofNullable(submodelElement.getSemanticId())
+                                .orElse(new DefaultReference()),
+                        "modelingType", modelingType,
+                        "value", children))
+                .dataAddress(dataAddress)
+                .build();
+    }
+
+    private <T extends SubmodelElement> Collection<SubmodelElement> getContainerElements(T submodelElement) {
+        if (submodelElement instanceof SubmodelElementCollection) {
+            return ((SubmodelElementCollection) submodelElement).getValue();
+        } else if (submodelElement instanceof SubmodelElementList) {
+            return ((SubmodelElementList) submodelElement).getValue();
+        } else { // Can not have any child elements...
+            return Collections.emptyList();
+        }
+    }
+}
---
+++ edc-extension4aas/src/main/java/de/fraunhofer/iosb/app/aas/mapper/SubmodelMapper.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2021 Fraunhofer IOSB, eine rechtlich nicht selbstaendige
+ * Einrichtung der Fraunhofer-Gesellschaft zur Foerderung der angewandten
+ * Forschung e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.fraunhofer.iosb.app.aas.mapper;
+
+import de.fraunhofer.iosb.dataplane.aas.spi.AasDataAddress;
+import org.eclipse.digitaltwin.aas4j.v3.model.KeyTypes;
+import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultReference;
+import org.eclipse.edc.connector.controlplane.asset.spi.domain.Asset;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class SubmodelMapper extends ElementMapper implements Mapper<Submodel> {
+
+    private final SubmodelElementMapper submodelElementMapper = new SubmodelElementMapper();
+    private final Supplier<Boolean> onlySubmodelsDecision;
+
+    public SubmodelMapper(Supplier<Boolean> onlySubmodelsDecision) {
+        super();
+        this.onlySubmodelsDecision = onlySubmodelsDecision;
+    }
+
+    @Override
+    public Asset map(Submodel submodel, String accessUrl) {
+        var reference = createReference(KeyTypes.SUBMODEL, submodel.getId());
+        Collection<Asset> children = new ArrayList<>();
+        if (!onlySubmodelsDecision.get()) {
+            children = submodel.getSubmodelElements().stream()
+                    .map(elem -> submodelElementMapper.map(reference, elem, accessUrl))
+                    .toList();
+        }
+
+        var semanticId = Objects.requireNonNullElse(
+                submodel.getSemanticId(),
+                new DefaultReference.Builder().build());
+
+        var dataAddress = AasDataAddress.Builder.newInstance()
+                .baseUrl(accessUrl)
+                .referenceChain(reference)
+                .build();
+
+        return mapIdentifiableToAssetBuilder(submodel)
+                .id(getId(accessUrl, dataAddress))
+                .properties(Map.of(
+                        "semanticId", semanticId,
+                        "submodelElements", children))
+                .dataAddress(dataAddress)
+                .build();
+    }
+}

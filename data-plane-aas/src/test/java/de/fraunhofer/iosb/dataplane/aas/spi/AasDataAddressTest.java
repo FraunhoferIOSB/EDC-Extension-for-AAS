51c230d61242fb93a00d94b2578ddbc14446468d
carlos-schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-10-06 14:49:25 +0200

    Add more tests

--- data-plane-aas/src/test/java/de/fraunhofer/iosb/dataplane/aas/spi/AasDataAddressTest.java
+++ data-plane-aas/src/test/java/de/fraunhofer/iosb/dataplane/aas/spi/AasDataAddressTest.java
@@ -2,11 +2,18 @@ package de.fraunhofer.iosb.dataplane.aas.spi;
 
 import de.fraunhofer.iosb.model.aas.AasProvider;
 import de.fraunhofer.iosb.model.aas.net.AasAccessUrl;
+import de.fraunhofer.iosb.util.Encoder;
+import org.eclipse.digitaltwin.aas4j.v3.model.Key;
+import org.eclipse.digitaltwin.aas4j.v3.model.KeyTypes;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultKey;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultReference;
 import org.eclipse.edc.spi.EdcException;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -51,4 +58,67 @@ class AasDataAddressTest {
         assertEquals(address.getPath(), "/path/to/resource");
     }
 
+    @Test
+    void test_build_returnCorrectReferenceChainAsPathNested() {
+        String smIdShort = "sm";
+
+        var keys = new ArrayList<Key>();
+        String path = "/submodel-elements/";
+        for (int i = 0; i < 100; i++) {
+            keys.add(getKey(KeyTypes.SUBMODEL_ELEMENT_COLLECTION, "smc%s".formatted(i)));
+            path = path.concat("smc%s.".formatted(i));
+
+        }
+        keys.add(getKey(KeyTypes.SUBMODEL_ELEMENT, "sme"));
+        path = path.concat("sme");
+
+        var referenceChain = new DefaultReference.Builder()
+                .keys(keys)
+                .build();
+
+        assertEquals(path.formatted(Encoder.encodeBase64(smIdShort)),
+                AasDataAddress.Builder.newInstance().referenceChain(referenceChain).build().getPath());
+    }
+
+    @Test
+    void test_build_returnCorrectReferenceChainAsPathSubmodel() {
+        String smIdShort = "sm";
+        var referenceChain = new DefaultReference.Builder()
+                .keys(List.of(
+                        getKey(KeyTypes.SUBMODEL, smIdShort)
+                ))
+                .build();
+
+        assertEquals("submodels/%s".formatted(Encoder.encodeBase64(smIdShort)),
+                AasDataAddress.Builder.newInstance().referenceChain(referenceChain).build().getPath());
+    }
+
+    @Test
+    void test_build_returnCorrectReferenceChainAsPathShell() {
+        String shellIdShort = "shell";
+        var referenceChain = new DefaultReference.Builder()
+                .keys(List.of(getKey(KeyTypes.ASSET_ADMINISTRATION_SHELL, shellIdShort)))
+                .build();
+
+        assertEquals("shells/%s".formatted(Encoder.encodeBase64(shellIdShort)),
+                AasDataAddress.Builder.newInstance().referenceChain(referenceChain).build().getPath());
+    }
+
+    @Test
+    void test_build_returnCorrectReferenceChainAsPathConceptDescription() {
+        String cdIdShort = "cd";
+        var referenceChain = new DefaultReference.Builder()
+                .keys(List.of(getKey(KeyTypes.CONCEPT_DESCRIPTION, cdIdShort)))
+                .build();
+
+        assertEquals("concept-descriptions/%s".formatted(Encoder.encodeBase64(cdIdShort)),
+                AasDataAddress.Builder.newInstance().referenceChain(referenceChain).build().getPath());
+    }
+
+    private Key getKey(KeyTypes keyType, String idShort) {
+        return new DefaultKey.Builder()
+                .type(keyType)
+                .value(idShort)
+                .build();
+    }
 }

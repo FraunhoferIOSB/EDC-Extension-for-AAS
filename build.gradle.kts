6f078bbf122377f1c32d7086ab4ffed79a505b84
carlos-schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2026-08-28 12:43:55 +0200

    feat: update ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,28 @@ allprojects {
     repositories.findByName("MavenLocal")?.let { repositories.remove(it) }
 }
 
+val testProvider = tasks.register("testSystemProvider", Exec::class) {
+    description = "Execute the provider system test"
+    commandLine("${rootProject.projectDir}/system-tests/provider/test.sh")
+}
+
+val testAasDataPlane = tasks.register("testSystemAasDataPlane", Exec::class) {
+    description = "Execute the AAS data plane system test"
+    commandLine("${rootProject.projectDir}/system-tests/aas-data-plane/test.sh")
+    dependsOn(testProvider) // Wait for the provider test to finish
+}
+
+val testStandalone = tasks.register("testSystemStandalone", Exec::class) {
+    description = "Execute the standalone system test"
+    commandLine("${rootProject.projectDir}/system-tests/standalone/test.sh")
+    dependsOn(testAasDataPlane) // Wait for the AAS data plane test to finish
+}
+
+tasks.register("testSystem", Task::class) {
+    description = "Run all system tests sequentially"
+    dependsOn(testStandalone) // Running standalone will automatically trigger the whole chain
+}
+
 tasks.check {
     dependsOn(tasks.named<JacocoReport>("testCodeCoverageReport"))
 }

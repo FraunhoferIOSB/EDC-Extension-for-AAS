47dfb9312fb74200e69a8e3a4a0ce96068336c79
carlos-schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2026-09-01 09:54:52 +0200

    fix: spotless

--- extensions/control-plane/edc-extension4aas/src/test/java/de/fraunhofer/iosb/app/controller/ConfigurationControllerTest.java
+++ extensions/control-plane/edc-extension4aas/src/test/java/de/fraunhofer/iosb/app/controller/ConfigurationControllerTest.java
@@ -19,12 +19,10 @@ import de.fraunhofer.iosb.aas.test.FileManager;
 import de.fraunhofer.iosb.app.model.configuration.Configuration;
 import org.eclipse.edc.spi.monitor.ConsoleMonitor;
 import org.eclipse.edc.spi.system.configuration.ConfigFactory;
-import org.eclipse.edc.web.spi.exception.InvalidRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
